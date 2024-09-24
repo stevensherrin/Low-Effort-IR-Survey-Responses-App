db5231be45333dbc6e25145aca90dd1b3d27e6f9
stevensherrin <33187047+stevensherrin@users.noreply.github.com>
2024-09-24 15:30:23 -0400

    troubleshooting
    app would not export Excel file.

--- deploy_shinylive.R
+++ deploy_shinylive.R
@@ -3,13 +3,13 @@ library(fs)
 #sessionInfo()
 
 # Define the path to your existing Shiny app
-existing_app_path <- "C:/Users/sherrins/OneDrive - Wentworth Institute of Technology/Desktop/test - low effort - Copy/myapp"
+existing_app_path <- "C:/Users/sherrins/OneDrive - Wentworth Institute of Technology/Documents/GitHub/Low-Effort-IR-Survey-Responses-App"
 
-shinylive::export(appdir = existing_app_path, destdir = "C:/Users/sherrins/OneDrive - Wentworth Institute of Technology/Desktop/test - low effort - Copy/myapp/docs")
+shinylive::export(appdir = existing_app_path, destdir = "C:/Users/sherrins/OneDrive - Wentworth Institute of Technology/Documents/GitHub/Low-Effort-IR-Survey-Responses-App/docs")
 
 #httpuv::runStaticServer("docs")
 
-httpuv::runStaticServer("C:/Users/sherrins/OneDrive - Wentworth Institute of Technology/Desktop/test - low effort - Copy/myapp/docs")
+httpuv::runStaticServer("C:/Users/sherrins/OneDrive - Wentworth Institute of Technology/Documents/GitHub/Low-Effort-IR-Survey-Responses-App/docs")
 
 #httpuv::runStaticServer("docs/", port=8008)
 
